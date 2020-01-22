6601f899a5babd0fcc5ba917481a0d9561e28958
Alexander Novikau <ramram81ta@gmail.com>
2020-01-22 16:21:00 +0300

    package import example

---
+++ Chapter 2.6/tempconv.go
@@ -0,0 +1,22 @@
+// temperature conversion package
+
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FrizingC      Celsius = 0
+	BoilingC      Celsius = 100
+
+	FrizingK Kelvin = 273.15
+	BoilingK Kelvin = 373.15
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%goC", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%goF", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%goK", k) }
---
+++ ConvMain/main.go
@@ -0,0 +1,11 @@
+package main
+import(
+	"fmt"
+	"github.com/novikov1981/training/tempconv Ch 2.6"
+	//"os"
+	//"strconv"
+)
+
+func main()  {
+	fmt.Println(tempconv.AbsoluteZeroC)
+}
